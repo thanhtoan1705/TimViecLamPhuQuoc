d70ddb1bf8375455448828ceb9f761478eb9f3bc
thanhtoan1705 <153188343+thanhtoan1705@users.noreply.github.com>
2024-07-24 20:14:24 +0700

    Build src

---
+++ app/Http/Controllers/Client/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller{
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
+    public function index(){
+        return view("client.home");
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,46 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function (){
+            $adminRoutes = [
+
+            ];
+
+            $clientRoutes = [
+                'client.php',
+                'user.php',
+                'employer.php',
+                'candidate.php',
+                'post.php',
+            ];
+
+
+            foreach ($adminRoutes as $route){
+                Route::middleware('web')
+                     ->prefix('admin')
+                     ->name('admin.')
+                     ->group(base_path("routes/admin/{$route}"));
+            }
+
+            foreach ($clientRoutes as $route){
+                Route::middleware('web')
+                     ->prefix('')
+                     ->group(base_path("routes/client/{$route}"));
+            }
+        },
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware){
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions){
         //
     })->create();
+
