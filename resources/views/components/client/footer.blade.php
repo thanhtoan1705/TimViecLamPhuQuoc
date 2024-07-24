73642f4354089255d1e8fc76d3ef97d951a27181
thanhtoan1705 <153188343+thanhtoan1705@users.noreply.github.com>
2024-07-24 21:36:31 +0700

    feat - Tạo component header và footer client

---
+++ resources/views/components/client/footer.blade.php
@@ -0,0 +1,63 @@
+
+
+
+
+<footer class="footer mt-50">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col-1 col-md-3 col-sm-12"><a href='index.html'><img alt="jobBox" src="{{ asset('assets/client//imgs/template/jobhub-logo.svg')}}"></a>
+          <div class="mt-20 mb-20 font-xs color-text-paragraph-2">JobBox là trung tâm của cộng đồng thiết kế và là nguồn tài nguyên tốt nhất để khám phá và kết nối với các nhà thiết kế và công việc trên toàn thế giới.</div>
+          <div class="footer-social"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
+        </div>
+        <div class="footer-col-2 col-md-2 col-xs-6">
+          <h6 class="mb-20">Tài nguyên</h6>
+          <ul class="menu-footer">
+            <li><a href="#">Về chúng tôi</a></li>
+            <li><a href="#">Đội của chúng tôi</a></li>
+            <li><a href="#">Các việc làm</a></li>
+            <li><a href="#">Liên hệ</a></li>
+          </ul>
+        </div>
+        <div class="footer-col-3 col-md-2 col-xs-6">
+          <h6 class="mb-20">Cộng đồng</h6>
+          <ul class="menu-footer">
+            <li><a href="#">Đánh giá</a></li>
+            <li><a href="#">Tính năng</a></li>
+            <li><a href="#">Tín dụng</a></li>
+            <li><a href="#">Câu hỏi thường gặp</a></li>
+          </ul>
+        </div>
+        <div class="footer-col-4 col-md-2 col-xs-6">
+          <h6 class="mb-20">Hồ sơ và CV</h6>
+          <ul class="menu-footer">
+            <li><a href="#">Quản lý CV của bạn</a></li>
+            <li><a href="#">Hướng dẫn viết CV</a></li>
+            <li><a href="#">Top CV Profile</a></li>
+            <li><a href="#">Thư viện CV</a></li>
+          </ul>
+        </div>
+        <div class="footer-col-5 col-md-2 col-xs-6">
+          <h6 class="mb-20">Xây dựng sự nghiếp</h6>
+          <ul class="menu-footer">
+            <li><a href="#">Việc làm tốt nhất</a></li>
+            <li><a href="#">Việt làm lương cao</a></li>
+            <li><a href="#">Việc làm  IT</a></li>
+            <li><a href="#">Việc làm quản lý</a></li>
+          </ul>
+        </div>
+        <div class="footer-col-6 col-md-3 col-sm-12">
+          <h6 class="mb-20">Tải ứng dụng</h6>
+          <p class="color-text-paragraph-2 font-xs">Tải xuống Ứng dụng của chúng tôi và được Giảm thêm 15% cho Đơn hàng đầu tiên của bạn…!&mldr;!</p>
+          <div class="mt-15"><a class="mr-5" href="#"><img src="{{ asset('assets/client//imgs/template/icons/app-store.png')}}" alt="joxBox"></a><a href="#"><img src="{{ asset('assets/client//imgs/template/icons/android.png')}}" alt="joxBox"></a></div>
+        </div>
+      </div>
+      <div class="footer-bottom mt-50">
+        <div class="row">
+          <div class="col-md-6"><span class="font-xs color-text-paragraph">Copyright © 2022. JobBox bảo lưu mọi quyền</span></div>
+          <div class="col-md-6 text-md-end text-start">
+            <div class="footer-social"><a class="font-xs color-text-paragraph" href="#">Chính sách bảo mật</a><a class="font-xs color-text-paragraph mr-30 ml-30" href="#">Điều khoản & điều kiện</a><a class="font-xs color-text-paragraph" href="#">Bảo mật</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/components/client/header.blade.php
@@ -0,0 +1,117 @@
+<header class="header sticky-bar">
+    <div class="container">
+        <div class="main-header">
+            <div class="header-left">
+                <div class="header-logo"><a class='d-flex' href='index.html'><img alt="jobBox"
+                            src="{{ asset('assets/client//imgs/template/jobhub-logo.svg') }}"></a></div>
+            </div>
+            <div class="header-nav">
+                <nav class="nav-main-menu">
+                    <ul class="main-menu">
+                        <li><a class='active' href='index.html'>Trang chủ</a>
+                        </li>
+                        <li><a href='jobs-grid.html'>Việc làm</a>
+                        </li>
+                        <li><a href='companies-grid.html'>Công ty</a>
+                        </li>
+                        <li><a href='candidates-grid.html'>Tin tức</a>
+                        </li>
+                        <li><a href='blog-grid.html'>Giới thiệu</a>
+                        </li>
+                        <li><a href='blog-grid.html'>Bảng giá</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="burger-icon burger-icon-white"><span class="burger-icon-top"></span><span
+                        class="burger-icon-mid"></span><span class="burger-icon-bottom"></span></div>
+            </div>
+            <div class="header-right">
+                <div class="block-signin"><a class='btn btn-default btn-shadow ml-40 hover-up' href='page-signin.html'>Đăng nhập</a></div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<div class="mobile-header-active mobile-header-wrapper-style perfect-scrollbar">
+    <div class="mobile-header-wrapper-inner">
+        <div class="mobile-header-content-area">
+            <div class="perfect-scroll">
+                <div class="mobile-search mobile-header-border mb-30">
+                    <form action="#">
+                        <input type="text" placeholder="Search…"><i class="fi-rr-search"></i>
+                    </form>
+                </div>
+                <div class="mobile-menu-wrap mobile-header-border">
+                    <!-- mobile menu start-->
+                    <nav>
+                        <ul class="mobile-menu font-heading">
+                            <li class="has-children"><a class='active' href='index.html'>Trang chủ</a>
+                            </li>
+                            <li class="has-children"><a href='jobs-grid.html'>Việc làm</a>
+                            </li>
+                            <li class="has-children"><a href='companies-grid.html'>Công ty</a>
+                            </li>
+                            <li class="has-children"><a href='candidates-grid.html'>Bài viết</a>
+                            </li>
+                            <li class="has-children"><a href='blog-grid.html'>Giới thiệu</a>
+                            </li>
+                            <li class="has-children"><a href='blog-grid.html'>Bảng giá</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="mobile-account">
+                    <h6 class="mb-10">Tài khoản của bạn</h6>
+                    <ul class="mobile-menu font-heading">
+                        <li><a href="#">Hồ hơ</a></li>
+                        <li><a href="#">Công việc</a></li>
+                        <li><a href="#">Cài đặt tài khoản</a></li>
+                        <li><a href='page-signin.html'>Đăng xuất</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="mobile-header-active mobile-header-wrapper-style perfect-scrollbar">
+    <div class="mobile-header-wrapper-inner">
+        <div class="mobile-header-content-area">
+            <div class="perfect-scroll">
+                <div class="mobile-search mobile-header-border mb-30">
+                    <form action="#">
+                        <input type="text" placeholder="Search…"><i class="fi-rr-search"></i>
+                    </form>
+                </div>
+                <div class="mobile-menu-wrap mobile-header-border">
+                    <!-- mobile menu start-->
+                    <nav>
+                        <ul class="mobile-menu font-heading">
+                            <li class="has-children"><a class='active' href='index.html'>Trang chủ</a>
+                            </li>
+                            <li class="has-children"><a href='jobs-grid.html'>Việc làm</a>
+                            </li>
+                            <li class="has-children"><a href='companies-grid.html'>Công ty</a>
+                            </li>
+                            <li class="has-children"><a href='candidates-grid.html'>Bài viết</a>
+                            </li>
+                            <li class="has-children"><a href='blog-grid.html'>Giới thiệu</a>
+                            </li>
+                            <li class="has-children"><a href='blog-grid.html'>Bảng giá</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="mobile-account">
+                    <h6 class="mb-10">Tài khoản của bản</h6>
+                    <ul class="mobile-menu font-heading">
+                        <li><a href="#">Hồ sơ</a></li>
+                        <li><a href="#">Công việc</a></li>
+                        <li><a href="#">Cài đặt tài khoản</a></li>
+                        <li><a href='page-signin.html'>Đăng xuất</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
